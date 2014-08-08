d909f853b35e01b3e8ce2de533e963477397e2b0
Aoede Pando & Ellen Krause Lehman <student@epicodus.com>
2014-08-08 15:03:41 -0700

    it saves authors to the authors array

--- lib/authors.rb
+++ lib/authors.rb
@@ -7,6 +7,21 @@ class Author
   end
 
   def Author.all
-    []
+    authors =[]
+    results = DB.exec("SELECT * FROM authors;")
+    results.each do |result|
+      name = result['name']
+      id = result['id'].to_i
+      authors <<Author.new({'name' => name, 'id' => id})
+    end
+    authors
+  end
+
+  def save
+    DB.exec("INSERT INTO authors (name, id) VALUES ('#{@name}', '#{@id}')")
+  end
+
+  def ==(another_book)
+    self.name == another_book.name
   end
 end
--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -10,4 +10,17 @@ describe 'Author' do
     new_author = Author.new({'name' => 'Socrates', 'id' => 2})
     expect(Author.all).to eq []
   end
+
+  it 'saves authors to the authors array' do
+    new_author = Author.new({'name' => 'Socrates', 'id' => 2})
+    new_author.save
+    expect(Author.all).to eq [new_author]
+  end
+
+  it 'is the same if the author name is the same' do
+    new_author = Author.new({'name' => 'Socrates', 'id' => 2})
+    new_author.save
+    new_author_two = Author.new({'name' => 'Socrates', 'id' => 3})
+    expect(new_author).to eq new_author_two
+  end
 end
