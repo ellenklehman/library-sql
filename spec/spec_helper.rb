bffc137f78b674dc40f53e86d4e7696494a9198f
Aoede Pando & Ellen Krause Lehman <student@epicodus.com>
2014-08-08 14:41:46 -0700

    initialize author with a name and id

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'authors'
+require 'pg'
+
+DB = PG.connect({:dbname => 'library_database'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM books *;")
+    DB.exec("DELETE FROM authors *;")
+  end
+end
